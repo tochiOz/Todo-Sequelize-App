457e52d1c37b3b2fd7bf9ccb7077928cbbc98d60
Tueloper <ozurumbatochukwu@yahoo.com>
2020-02-06 20:13:23 +0100

    TodoItem route handlers on point

--- Documents/htdocs/Node/Node_Sequelize/src/controllers/todoitems.js
+++ Documents/htdocs/Node/Node_Sequelize/src/controllers/todoitems.js
@@ -12,5 +12,60 @@ module.exports = {
 		} catch (e) {
 			return res.status(400).send(e.message);
 		}
+	},
+
+	async updateTodoItem(req, res) {
+		try {
+			const isupdateTodoItem = await TodoItem.findOne({
+				where: {
+					id: req.params.todoItemId,
+					todoId: req.params.todoId
+				}
+			});
+
+			if (!isupdateTodoItem) {
+				return res.status(404).send({
+					message: 'TodoItem Not Found!!!!!!!!!!!!'
+				});
+			} else {
+				const updatedTodoItem = await isupdateTodoItem.update({
+					content: req.body.content || todo.content,
+					complete: req.body.complete || todo.complete
+				});
+
+				return res.status(200).send({
+					message: 'TodoItem Updated Successfully',
+					updatedTodoItem
+				});
+			}
+		} catch (e) {
+			return res.status(400).send(e.message);
+		}
+	},
+
+	async destroySingleTodoItem(req, res) {
+		try {
+			const destroyableTodoItem = await TodoItem.findOne({
+				where: {
+					id: req.params.todoItemId,
+					todoId: req.params.todoId
+				}
+			});
+
+			if (!destroyableTodoItem) {
+				return res.status(404).send({
+					message: 'Todo Not Found!!!!!!!!!!!!'
+				});
+			} else {
+				const destroyedTodoItem = await destroyableTodoItem.destroy();
+
+				return res.status(200).send({
+					destroyedTodoItem,
+					message: 'Todo Deleted Succefulyy'
+				});
+			}
+		} catch (e) {
+			return res.status(400).send(e.message);
+		}
 	}
 };
--- Documents/htdocs/Node/Node_Sequelize/src/routes/todoitem.js
+++ Documents/htdocs/Node/Node_Sequelize/src/routes/todoitem.js
@@ -1,12 +1,15 @@
 import { Router } from 'express';
-import { createTodoItem, listTodoItems } from './../controllers/todoitems';
+import { createTodoItem, updateTodoItem, destroySingleTodoItem } from './../controllers/todoitems';
 
 const router = new Router();
 
 //routes
 router.post('/create/:todoId/todoitems', createTodoItem);
 
-//get all todo tites
-// router.get('/todos/todoitems', listTodoItems);
+//update single todo items
+router.patch('/todo/:todoId/todoItem/:todoItemId', updateTodoItem);
+
+//update single todo items
+router.delete('/todo/:todoId/todoItem/:todoItemId', destroySingleTodoItem);
 
 export default router;
