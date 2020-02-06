b753c8d565d8d633fa8f8582ed8728eb783a3858
Tueloper <ozurumbatochukwu@yahoo.com>
2020-02-06 03:19:00 +0100

    setting routes

---
+++ Documents/htdocs/Node/Node_Sequelize/src/app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import morgan from 'morgan';
+
+//importing routes
+import projectRoutes from './routes/projects';
+import taskRoutes from './routes/task';
+
+const app = express();
+
+//middlewares
+app.use(morgan('dev'));
+app.use(json());
+
+//routes
+app.use(projectRoutes);
+app.use(taskRoutes);
+
+export default app;
